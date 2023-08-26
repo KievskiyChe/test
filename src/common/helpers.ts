8de525405208b53369336d7ff5f69326c5788292
Ernest Isaev <eriscrypt@gmail.com>
2023-08-27 02:53:18 +0300

    update calc

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -82,12 +82,10 @@ export const calculateSlippage = (
   slippage: string | number, // user slippage input
   decimals: number // token decimals
 ) => {
-  console.log(amount, slippage, decimals)
   const _amount = BigNumber.from(parseUnits(parseFloat(amount).toFixed(2), decimals))
   const _slippage = 1 - Number(slippage) / 100
   const result = Number(_amount) * Number(_slippage)
-  console.log(_amount, _slippage)
-  return Math.floor(result).toString();
+  return Math.floor(result).toLocaleString('fullwide', {useGrouping:false})
 };
 
 export const switchNetwork = (chainId: number) => {
